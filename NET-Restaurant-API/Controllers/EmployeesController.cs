76ee3ed5278778ef4ef18c36d2eeafd8e33f489b
Alexandru-Antonio Petrescu <apetrescu@adobe.com>
2023-08-01 16:12:32 +0300

    Created the GET methods by id for the EmployeesController

--- NET-Restaurant-API/Controllers/EmployeesController.cs
+++ NET-Restaurant-API/Controllers/EmployeesController.cs
@@ -9,19 +9,56 @@ namespace NET_Restaurant_API.Controllers
 	{
 		public static List<Employee> employees = new List<Employee>
 		{
-			new Employee{ Id = 1, FirstName = "Employee1FirstName", LastName = "Employee1LastName" },
-			new Employee{ Id = 2, FirstName = "Employee2FirstName", LastName = "Employee2LastName" },
-			new Employee{ Id = 3, FirstName = "Employee3FirstName", LastName = "Employee3LastName" },
-			new Employee{ Id = 4, FirstName = "Employee4FirstName", LastName = "Employee4LastName" },
-			new Employee{ Id = 5, FirstName = "Employee5FirstName", LastName = "Employee5LastName" }
+			new Employee{ Id = 1, FirstName = "Employee1FirstName", LastName = "Employee1LastName", Age = 2 },
+			new Employee{ Id = 2, FirstName = "Employee2FirstName", LastName = "Employee2LastName", Age = 18},
+			new Employee{ Id = 3, FirstName = "Employee3FirstName", LastName = "Employee3LastName", Age = 5 },
+			new Employee{ Id = 4, FirstName = "Employee4FirstName", LastName = "Employee4LastName", Age = 20 },
+			new Employee{ Id = 5, FirstName = "Employee5FirstName", LastName = "Employee5LastName", Age = 16 }
 		};
 
 		// Endpoint
+		// GET
 		[HttpGet]
 		public List<Employee> Get()
 		{
 			return employees;
 		}
-	} 
+
+		[HttpGet("byId")]
+		public Employee GetById(int id)
+		{
+			return employees.FirstOrDefault(x => x.Id.Equals(id));
+		}
+
+        [HttpGet("byId/{id}")]
+        public Employee GetByIdInEndpoint(int id)
+        {
+            return employees.FirstOrDefault(x => x.Id.Equals(id));
+        }
+
+		[HttpGet("filter/{name}/{age}")]
+		public List<Employee> GetWithFilters(string name, int age)
+		{
+			return employees.Where(s => (s.FirstName + " " + s.LastName).ToLower().Equals(name.ToLower()) && s.Age > age).ToList();
+		}
+
+		[HttpGet("fromRouteWithId/{id}")]
+		public Employee GetByIdFromRoute([FromRoute] int id)
+		{
+            return employees.FirstOrDefault(x => x.Id.Equals(id));
+        }
+
+        [HttpGet("fromHeader")]
+		public Employee GetByIdFromHeader([FromHeader] int id)
+		{
+            return employees.FirstOrDefault(x => x.Id.Equals(id));
+        }
+
+		[HttpGet("fromQuery")]
+		public Employee GetByIdFromQuery([FromQuery] int id)
+		{
+            return employees.FirstOrDefault(x => x.Id.Equals(id));
+        }
+    } 
 }
 
--- NET-Restaurant-API/Models/Employee.cs
+++ NET-Restaurant-API/Models/Employee.cs
@@ -3,9 +3,9 @@
 	public class Employee
 	{
 		public int Id { get; set; }
-		public string? FirstName { get; set; }
-		public string? LastName { get; set; }
-		public int age { get; set; }
+		public string FirstName { get; set; }
+		public string LastName { get; set; }
+		public int Age { get; set; }
 	}
 }
 
